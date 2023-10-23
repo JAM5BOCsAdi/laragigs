109b1194f6c147dcd8b84b3fafbe4095e20901be
CsAdi <oradam86@gmail.com>
2023-10-23 15:11:09 +0200

    01:34:50 : Re-usable Components[card.blade.php, listing-card.blade.php] | Implement styling[html] using Tailwind

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,18 +1,23 @@
-<h1>{{ $heading }}</h1>
-{{--
+@extends('layout')
+
+@section('content')
+    @include('partials._hero')
+    @include('partials._search')
+    {{-- <h1>{{ $heading }}</h1> --}}
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        {{--
 @php
     $test = 1;
 @endphp
 --}}
-{{-- {{ $test }} --}}
-@unless (count($listings) == 0)
-    @foreach ($listings as $listing)
-        <h2>
-            <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}
-            </a>
-        </h2>
-        <p>{{ $listing['description'] }}</p>
-    @endforeach
-@else
-    <p>No listings found</p>
-@endunless
+        {{-- {{ $test }} --}}
+        @unless (count($listings) == 0)
+            @foreach ($listings as $listing)
+                <!-- Item -->
+                <x-listing-card :listing="$listing" />
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endunless
+    </div>
+@endsection
