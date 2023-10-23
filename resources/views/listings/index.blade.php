e2d12b836d0b8b7132b1248da5c4f36b8c7c754c
CsAdi <oradam86@gmail.com>
2023-10-23 16:52:29 +0200

    01:48:40 : Another way of using 'layout.blade.php' as a generic/re-usable, and implement it with <x-layout> and not with @extends and @section

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,23 +1,22 @@
-@extends('layout')
-
-@section('content')
-    @include('partials._hero')
-    @include('partials._search')
-    {{-- <h1>{{ $heading }}</h1> --}}
-    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-        {{--
+<x-layout>
+    @section('content')
+        @include('partials._hero')
+        @include('partials._search')
+        {{-- <h1>{{ $heading }}</h1> --}}
+        <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+            {{--
 @php
     $test = 1;
 @endphp
 --}}
-        {{-- {{ $test }} --}}
-        @unless (count($listings) == 0)
-            @foreach ($listings as $listing)
-                <!-- Item -->
-                <x-listing-card :listing="$listing" />
-            @endforeach
-        @else
-            <p>No listings found</p>
-        @endunless
-    </div>
-@endsection
+            {{-- {{ $test }} --}}
+            @unless (count($listings) == 0)
+                @foreach ($listings as $listing)
+                    <!-- Item -->
+                    <x-listing-card :listing="$listing" />
+                @endforeach
+            @else
+                <p>No listings found</p>
+            @endunless
+        </div>
+    </x-layout>
--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,6 +1,7 @@
-@extends('layout')
+<x-layout>
 
-@section('content')
+
+
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
@@ -40,4 +41,4 @@
             </div>
         </x-card>
     </div>
-@endsection
+</x-layout>
