e5c424ca79ea319d4db5640887d7199b2da9ead5
CsAdi <oradam86@gmail.com>
2023-10-25 17:15:30 +0200

    Connect User and Listing models

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // Relationship with Listing
+    public function listings()
+    {
+        return $this->hasMany(User::class, 'user_id');
+    }
 }
